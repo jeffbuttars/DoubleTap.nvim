0e1624d878b670147e38cca0058abdf67e11d1ca
Jeff Buttars <jeffbuttars@gmail.com>
2023-11-09 09:02:41 -0700

    Added a defaults.lua and moved the configuration default values into there

--- lua/DoubleTap/config.lua
+++ lua/DoubleTap/config.lua
@@ -1,38 +1,5 @@
 local M = {}
-
-M.defaults = {
-	threshold = 0.4, -- In seconds
-	jump_out = {
-		{ key = ")", },
-		{ key = "}" },
-		{ key = "]" },
-	},
-	jump_in = {
-		{ key = "(", lhs = "( ", rhs = " )"},
-		{ key = "{", lhs = "{ ", rhs = " }" },
-		{ key = "[", lhs = "[", rhs = "]" },
-	},
-	jump_in_or_out = {
-		{ key = "'", lhs = "'", rhs = "'", out_condition = { "string", "string.documentation" } },
-		{ key = '"', lhs = '"', rhs = '"', out_condition = { "string", "string.documentation" } },
-		{ key = '`', lhs = "`", rhs = "`", out_condition = { "string", "string.documentation" } },
-	},
-
-  -- Enclosing/Surrounding character mappings, visually select then double tap the
-  -- character to enclose the selections
-  -- Add the `enable = false` key to disable a mapping
-  visual_surrounds = {
-    {keys = "((", map = "<ESC>`>a)<ESC>`<i(<ESC>"},
-    {keys = "))", map = "<ESC>`<i(<ESC>`><right>a)<ESC>"},
-    {keys = "{{", map = "<ESC>`>a}<ESC>`<i{<ESC>"},
-    {keys = "}}", map = "<ESC>`<i{<ESC>`><right>a}<ESC>"},
-    {keys = "[[", map = "<ESC>`>a]<ESC>`<i[<ESC>"},
-    {keys = "]]", map = "<ESC>`<i[<ESC>`><right>a]<ESC>"},
-    {keys = '""', map = '<ESC>`>a"<ESC>`<i"<ESC>'},
-    {keys = "''", map = "<ESC>`>a'<ESC>`<i'<ESC>"},
-    {keys = "``", map = "<ESC>`>a`<ESC>`<i`<ESC>"},
-  }
-}
+M.defaults = require("DoubleTap.defaults")
 
 M.setup_config = function(config)
 	if not (type(config) == "table") then
